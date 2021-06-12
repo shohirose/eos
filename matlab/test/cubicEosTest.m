e4385d016cb97e3ef3a2a8064a1b6f6790e9478b
Sho Hirose <sho.hirose@gmail.com>
2021-06-12 22:15:29 +0900

    test: Update unit tests to include molecular weight

--- matlab/test/cubicEosTest.m
+++ matlab/test/cubicEosTest.m
@@ -6,12 +6,13 @@ import eos.PengRobinsonEos
 Pc = 4e6;       % Critical pressure [Pa]
 Tc = 190.6;     % Critical temperature [K]
 omega = 0.008;  % Acentric factor
+Mw = 16.0425;   % Molecular weight [g/mol]
 
 P = 3.0e6; % Pressure [Pa]
 T = 180;   % Temperature [K]
 
 %% VanDerWaalsEos test:
-vdweos = VanDerWaalsEos(Pc,Tc);
+vdweos = VanDerWaalsEos(Pc,Tc,Mw);
 [z,A,B] = vdweos.zFactors(P,T);
 z = sort(z);
 assert(max(abs((z - [0.207498, 0.275339, 0.616434]')./z)) < 1e-3);
@@ -19,7 +20,7 @@ phi = vdweos.fugacityCoeff(z,A,B);
 assert(max(abs((phi - [0.756747, 0.758617, 0.741050]')./phi)) < 1e-3);
 
 %% SoaveRedlichKwongEos test:
-srkeos = SoaveRedlichKwongEos(Pc,Tc,omega);
+srkeos = SoaveRedlichKwongEos(Pc,Tc,omega,Mw);
 [z,A,B] = srkeos.zFactors(P,T);
 z = sort(z);
 assert(max(abs((z - [0.152443, 0.310673, 0.536884]')./z)) < 1e-3);
@@ -27,7 +28,7 @@ phi = srkeos.fugacityCoeff(z,A,B);
 assert(max(abs((phi - [0.69289, 0.70862, 0.70353]')./phi)) < 1e-3);
 
 %% PengRobinsonEos test:
-preos = PengRobinsonEos(Pc,Tc,omega);
+preos = PengRobinsonEos(Pc,Tc,omega,Mw);
 [z,A,B] = preos.zFactors(P,T);
 z = sort(z);
 assert(max(abs((z - [0.135628, 0.292355, 0.510231]')./z)) < 1e-3);
