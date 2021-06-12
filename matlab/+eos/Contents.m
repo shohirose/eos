4481fae206fefbfe571409faa9581aa705d70f30
Sho Hirose <sho.hirose@gmail.com>
2021-06-12 20:15:37 +0900

    doc: Create Contents.m for matlab help

---
+++ matlab/+eos/Contents.m
@@ -0,0 +1,8 @@
+% +EOS
+%
+% Files
+%   CubicEosBase           - Base class for two-parameter cubic equation of state.
+%   PengRobinsonEos        - Peng-Robinson equation of state
+%   SoaveRedlichKwongEos   - Soave-Redlich-Kwong equation of state
+%   ThermodynamicConstants - Class for thermodynamic constants
+%   VanDerWaalsEos         - Van der Waals equation of state
