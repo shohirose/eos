c32dcad4ea746741d0366976ad118ed4475f346d
Sho Hirose <sho.hirose@gmail.com>
2021-06-22 23:44:32 +0900

    test: Update tests for purecomp package

--- matlab/test/purecompTest.m
+++ matlab/test/purecompTest.m
@@ -1,6 +1,6 @@
-import eos.VanDerWaalsEos
-import eos.SoaveRedlichKwongEos
-import eos.PengRobinsonEos
+import eos.purecomp.VanDerWaalsEos
+import eos.purecomp.SoaveRedlichKwongEos
+import eos.purecomp.PengRobinsonEos
 
 % Methane
 Pc = 4e6;       % Critical pressure [Pa]
@@ -12,25 +12,25 @@ P = 3.0e6; % Pressure [Pa]
 T = 180;   % Temperature [K]
 
 %% VanDerWaalsEos test:
-vdweos = VanDerWaalsEos(Pc,Tc,Mw);
-[z,A,B] = vdweos.zFactors(P,T);
+eos = VanDerWaalsEos(Pc,Tc,Mw);
+[z,s] = eos.zFactors(P,T);
 z = sort(z);
 assert(max(abs((z - [0.207498, 0.275339, 0.616434]')./z)) < 1e-3);
-phi = vdweos.fugacityCoeff(z,A,B);
+phi = eos.fugacityCoeff(z,s);
 assert(max(abs((phi - [0.756747, 0.758617, 0.741050]')./phi)) < 1e-3);
 
 %% SoaveRedlichKwongEos test:
-srkeos = SoaveRedlichKwongEos(Pc,Tc,omega,Mw);
-[z,A,B] = srkeos.zFactors(P,T);
+eos = SoaveRedlichKwongEos(Pc,Tc,omega,Mw);
+[z,s] = eos.zFactors(P,T);
 z = sort(z);
 assert(max(abs((z - [0.152443, 0.310673, 0.536884]')./z)) < 1e-3);
-phi = srkeos.fugacityCoeff(z,A,B);
+phi = eos.fugacityCoeff(z,s);
 assert(max(abs((phi - [0.69289, 0.70862, 0.70353]')./phi)) < 1e-3);
 
 %% PengRobinsonEos test:
-preos = PengRobinsonEos(Pc,Tc,omega,Mw);
-[z,A,B] = preos.zFactors(P,T);
+eos = PengRobinsonEos(Pc,Tc,omega,Mw);
+[z,s] = eos.zFactors(P,T);
 z = sort(z);
 assert(max(abs((z - [0.135628, 0.292355, 0.510231]')./z)) < 1e-3);
-phi = preos.fugacityCoeff(z,A,B);
+phi = eos.fugacityCoeff(z,s);
 assert(max(abs((phi - [0.67210, 0.68819, 0.68362]')./phi)) < 1e-3);
