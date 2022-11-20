081b2d896f884e3b2c651666f92384e318c4e9e6
Sho Hirose <sho.hirose@gmail.com>
2022-11-20 21:05:46 +0900

    Update comments for CubicEosState

--- cpp/include/eos/cubic_eos_state.hpp
+++ cpp/include/eos/cubic_eos_state.hpp
@@ -11,27 +11,43 @@ struct CubicEosState {
  public:
   using Scalar = typename CubicEosTraits<Eos>::Scalar;
 
-  /// @param[in] a Reduced attraction parameter
-  /// @param[in] b Reduced repulstion parameter
+  /**
+   * @brief Construct a new Cubic Eos State object
+   * 
+   * @param a Reduced attraction parameter
+   * @param b Reduced repulstion parameter
+   */
   CubicEosState(const Scalar& a, const Scalar& b) : a_{a}, b_{b} {}
 
-  /// @brief Computes Z-factors
-  /// @tparam CubicEquationSolver Cubic equation solver
-  /// @param[in] solver Cubic equation solver
-  ///
-  /// A cubic equation solver takes the coefficients of the cubic equation as
-  /// std::array<Scalar, 3> and returns real roots as std::vector<Scalar>.
+  /**
+   * @brief Computes Z-factors
+   * @tparam CubicEquationSolver Cubic equation solver
+   * @param[in] solver Cubic equation solver
+   * @return std::vector<Scalar> Z-factors
+   *
+   * A cubic equation solver takes the coefficients of the cubic equation as
+   * std::array<Scalar, 3> and returns real roots as std::vector<Scalar>.
+   */
   template <typename CubicEquationSolver>
   std::vector<Scalar> zfactors(CubicEquationSolver&& solver) const {
     return solver(Eos::zfactor_cubic_eq(a_, b_));
   }
 
-  /// @param[in] z Z-factor
+  /**
+   * @brief Computes natural log of fugacity coefficient.
+   * @param[in] z Z-factor
+   * @return Scalar Natural log of fugacity coefficient
+   */
   Scalar ln_fugacity_coeff(const Scalar& z) const noexcept {
     return Eos::ln_fugacity_coeff(z, a_, b_);
   }
 
-  /// @param[in] z Z-factors
+  /**
+   * @brief Computes natural log of fuacity coefficients
+   * 
+   * @param z Z-factors
+   * @return std::vector<Scalar> Natural log of fugacity coefficients
+   */
   std::vector<Scalar> ln_fugacity_coeff(
       const std::vector<Scalar>& z) const noexcept {
     std::vector<Scalar> ln_phi(z.size());
@@ -41,12 +57,22 @@ struct CubicEosState {
     return ln_phi;
   }
 
-  /// @param[in] z Z-factor
+  /**
+   * @brief Computes fugacity coefficient.
+   * 
+   * @param z Z-factor
+   * @return Scalar Fugacity coefficient
+   */
   Scalar fugacity_coeff(const Scalar& z) const noexcept {
     return Eos::fugacity_coeff(z, a_, b_);
   }
 
-  /// @param[in] z Z-factors
+  /**
+   * @brief Computes fugacity coefficients.
+   * 
+   * @param z Z-factors
+   * @return std::vector<Scalar> Fugacity coefficients
+   */
   std::vector<Scalar> fugacity_coeff(
       const std::vector<Scalar>& z) const noexcept {
     std::vector<Scalar> phi(z.size());
